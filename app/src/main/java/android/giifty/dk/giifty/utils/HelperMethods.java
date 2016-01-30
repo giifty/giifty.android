c618191b1a048fef7edbeb0c15c13b636f19132b
Mads Kverneland <mak@MIR-MAK.Miracle.local>
2016-01-30 15:45:16 +0100

    startActivityWithHero animation added to helperMethods

--- app/src/main/java/android/giifty/dk/giifty/utils/HelperMethods.java
+++ app/src/main/java/android/giifty/dk/giifty/utils/HelperMethods.java
@@ -1,6 +1,11 @@
 package android.giifty.dk.giifty.utils;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.support.v4.app.ActivityOptionsCompat;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
@@ -13,5 +18,15 @@ public class HelperMethods {
 
     public static void setImage(Context context, ImageView imageView, String imageUrl) {
         Picasso.with(context).load(imageUrl).into(imageView);
+    }
+
+    public static void startActivityWithHero(Activity activity, View transitionView, Intent intent, String transitionName) {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+            ActivityOptionsCompat optionsCompat = ActivityOptionsCompat
+                    .makeSceneTransitionAnimation(activity, transitionView, transitionName);
+            activity.startActivity(intent, optionsCompat.toBundle());
+        }else {
+            activity.startActivity(intent);
+        }
     }
 }
