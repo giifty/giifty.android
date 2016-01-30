fec9c0806d2c4de39b0b28cf8c37ee2f1443c1fa
Mads Kverneland <mak@MIR-MAK.Miracle.local>
2016-01-30 16:51:32 +0100

    base activity added

---
+++ app/src/main/java/android/giifty/dk/giifty/components/BaseActivity.java
@@ -0,0 +1,34 @@
+package android.giifty.dk.giifty.components;
+
+import android.giifty.dk.giifty.R;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if(getSupportActionBar() != null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == android.R.id.home){
+            onBackPressed();
+        }
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        overridePendingTransition(R.animator.fade_in, R.animator.slide_out_right);
+    }
+}
