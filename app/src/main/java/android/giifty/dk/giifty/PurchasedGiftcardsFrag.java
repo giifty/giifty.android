f5fbbdc8b554088284119e4eba08dcaa392a940c
Mads Kverneland <mak@MIR-MAK.Miracle.local>
2016-03-24 11:44:10 +0100

    new update

--- app/src/main/java/android/giifty/dk/giifty/PurchasedGiftcardsFrag.java
+++ app/src/main/java/android/giifty/dk/giifty/PurchasedGiftcardsFrag.java
@@ -1,11 +1,15 @@
 package android.giifty.dk.giifty;
 
 
+import android.content.DialogInterface;
+import android.giifty.dk.giifty.broadcastreceivers.MyBroadcastReceiver;
 import android.giifty.dk.giifty.giftcard.GiftcardAdapter1;
 import android.giifty.dk.giifty.giftcard.GiftcardRepository;
 import android.giifty.dk.giifty.model.Giftcard;
+import android.giifty.dk.giifty.web.SignInHandler;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -13,6 +17,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.io.IOException;
 import java.util.List;
 
 
@@ -24,6 +29,7 @@ public class PurchasedGiftcardsFrag extends Fragment {
 
     private GiftcardRepository controller;
     private GiftcardAdapter1 adapter;
+    private SignInHandler signInHandler;
 
     public PurchasedGiftcardsFrag() {
         // Required empty public constructor
@@ -40,7 +46,7 @@ public class PurchasedGiftcardsFrag extends Fragment {
         adapter = new GiftcardAdapter1(getActivity(), list);
         TextView emptyText = (TextView) root.findViewById(R.id.no_giftcards_text_id);
 
-        if(list.isEmpty()){
+        if (list.isEmpty()) {
             emptyText.setText(getText(R.string.msg_no_puchased_gc));
         }
 
@@ -48,7 +54,49 @@ public class PurchasedGiftcardsFrag extends Fragment {
         recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2, GridLayoutManager.VERTICAL, false));
         recyclerView.setHasFixedSize(true);
         recyclerView.setAdapter(adapter);
+
+        signInHandler = new SignInHandler();
         return root;
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        f();
+        try {
+            if (signInHandler.refreshTokenAsync()) {
+                // Create user dialog
+            } else {
+                //create spinner running signIn
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    private void f() {
+        new AlertDialog.Builder(getContext())
+                .setTitle("fhjdsf")
+                .setCancelable(false)
+                .setNeutralButton("43", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                })
+                .show();
+    }
+
+    class MyReceiver extends MyBroadcastReceiver {
+
+        @Override
+        public void onSignIn() {
+            if (signInHandler.isTokenExpired()) {
+
+            }
+
+        }
+    }
 }
